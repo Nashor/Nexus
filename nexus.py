ed4fb3dde88f027a2f42375d0060dded01c0884d
Nashor <nashorsmind@gmail.com>
2019-01-08 17:27:20 -0500

    Removed defunct Nexus classes, added support for co-ordinates
    The now defunct Nexus classes (Nexus and Nexus2 from previous commits) were
    removed, as they are no longer needed. Nexus3 was renamed to Nexus. Support for
    co-ordinates was also added.

--- nexus.py
+++ nexus.py
@@ -1,79 +1,76 @@
+from collections import namedtuple
 from math import exp, log
 import numpy as np
 import matplotlib.pyplot as plt
 
+Coord = namedtuple('Coord', ['x', 'y', 'z'])
+
 class Nexus:
-   
-   def __init__(self, minfluence, maxfluence, radius):
-      self.minfluence = minfluence
-      self.maxfluence = maxfluence
-      self.radius = radius
-      
-      self.diffuse = -(radius * radius) / log(minfluence / maxfluence)
-   
-   def influence(self, dist):
-      if dist > self.radius:
-         return 0
-      else:
-         return self.maxfluence * exp(-(dist * dist) / self.diffuse)
-
-class Nexus2:
-   
-   def __init__(self, minfluence, maxfluence, radius):
-      self.minfluence = minfluence
-      self.maxfluence = maxfluence
-      self.radius = radius
-      
-      self.diffuse = -(radius * radius) / log(.1 / maxfluence)
-   
-   def influence(self, dist):
-      if dist > self.radius:
-         return 0
-      else:
-         return (self.maxfluence - self.minfluence + 1) * exp(-(dist * dist) / self.diffuse) + (self.minfluence - 1)
-
-class Nexus3:
    
    def __init__(self, minfluence, maxfluence, radius, diffuse):
       self.minfluence = minfluence
       self.maxfluence = maxfluence
       self.radius = radius
+      self.radius2 = radius**2
       self.diffuse = diffuse
+      self.coord = Coord(0, 0, 0)
       
       self.a = maxfluence - minfluence + diffuse
       self.b = minfluence - diffuse
-      self.c = (radius * radius) / log(diffuse / self.a)
+      self.c = (radius**2) / log(diffuse / self.a)
    
-   def influence(self, dist):
-      if dist > self.radius:
+   def dist2(self, coord):
+      '''Returns the distance squared from the Nexus co-ordinate to the
+      provided co-ordinate.
+      
+      The actual distance, which would be the square root of this result, is
+      not required. The distance from the Nexus co-ordinate to the provided 
+      co-ordinate is squared in the influence formula.'''
+      return (
+         (self.coord.x - coord.x)**2 + 
+         (self.coord.y - coord.y)**2 + 
+         (self.coord.z - coord.z)**2
+      )
+   
+   def influence(self, dist=0, coord=None):
+      '''Returns the influence the Nexus has on a co-ordinate. A distance can
+      also be provided directly.'''
+      if coord:
+         d2 = self.dist2(coord)
+      else:
+         d2 = dist**2
+         
+      if d2 > self.radius2:
          return 0
       else:
-         return self.a * exp((dist * dist) / self.c) + self.b
+         return self.a * exp(d2 / self.c) + self.b
+      
 
 def main():
    minfluence = 10
    maxfluence = 30
    radius = 20
    
-   nexus = Nexus(minfluence, maxfluence, radius)
-   nexus2 = Nexus2(minfluence, maxfluence, radius)
-   nexus3 = Nexus3(minfluence, maxfluence, radius, 0.01)
+   nexus1 = Nexus(minfluence, maxfluence, radius, 1)
+   nexus2 = Nexus(minfluence, maxfluence, radius, 0.1)
+   nexus3 = Nexus(minfluence, maxfluence, radius, 5)
    
-   assert nexus.influence(0) == maxfluence
-   assert nexus3.influence(0) == maxfluence
-   assert nexus.influence(radius) == minfluence
-   assert nexus3.influence(radius) == minfluence
-   assert nexus.influence(radius + 1) == 0
-   assert nexus3.influence(radius + 1) == 0
+   for nexus in [nexus1, nexus2, nexus3]:
+      assert nexus.influence(0) == maxfluence
+      assert nexus.influence(radius) == minfluence
+      assert nexus.influence(radius + 1) == 0
    
    x = np.arange(0, radius + 2, 0.1)
-   y1 = np.array([nexus.influence(xi) for xi in x])
-   y2 = np.array([nexus2.influence(xi) for xi in x])
-   y3 = np.array([nexus3.influence(xi) for xi in x])
+   y1 = np.array([nexus1.influence(dist=xi) for xi in x])
+   y2 = np.array([nexus2.influence(dist=xi) for xi in x])
+   y3 = np.array([nexus3.influence(dist=xi) for xi in x])
+   #y1 = np.array([nexus1.influence(coord=Coord(0,0,xi)) for xi in x])
+   #y2 = np.array([nexus2.influence(coord=Coord(0,0,xi)) for xi in x])
+   #y3 = np.array([nexus3.influence(coord=Coord(0,0,xi)) for xi in x])
    plt.plot(x, y1)
    plt.plot(x, y2)
    plt.plot(x, y3)
-   plt.legend(['Nexus', 'Nexus2', 'Nexus3'])
+   plt.legend(['Nexus1', 'Nexus2', 'Nexus3'])
    plt.show()
 
 if __name__ == '__main__':
