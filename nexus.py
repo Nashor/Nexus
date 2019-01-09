5550268f49cb09212ff3468503c9b7bdc4a876dc
Nashor <nashorsmind@gmail.com>
2019-01-09 14:46:07 -0500

    Added database class, and .gitignore
    A new class to store Nexus objects was created. The sqlite3 library from Python
    is used as the database to store the data. A .gitignore file was added to
    ignore the database file.

--- nexus.py
+++ nexus.py
@@ -2,8 +2,52 @@ from collections import namedtuple
 from math import exp, log
 import numpy as np
 import matplotlib.pyplot as plt
+import sqlite3
 
 Coord = namedtuple('Coord', ['x', 'y', 'z'])
+
+class Database:
+   
+   def __init__(self):
+      self.conn = sqlite3.connect('nexus.db')
+      self.c = self.conn.cursor()
+      
+      self.c.execute(
+         '''
+         CREATE TABLE IF NOT EXISTS nexus (
+            x int NOT NULL,
+            y int NOT NULL,
+            z int NOT NULL,
+            minfluence real,
+            maxfluence real,
+            radius real,
+            diffuse real,
+            CONSTRAINT id PRIMARY KEY (x, y, z)
+         );
+         '''
+      )
+      self.conn.commit()
+   
+   def clear(self):
+      self.c.execute('DELETE FROM nexus')
+      self.conn.commit()
+   
+   def get_nexus(self, coord):
+      self.c.execute('SELECT * FROM nexus WHERE x=? AND y=? AND z=?', coord)
+      tup = self.c.fetchone()
+      
+      nexus = Nexus(tup[3], tup[4], tup[5], tup[6])
+      nexus.coord = Coord(tup[0], tup[1], tup[2])
+      
+      return nexus
+   
+   def put_nexus(self, nexus):
+      self.c.execute(
+         'INSERT INTO nexus VALUES (?,?,?,?,?,?,?)',
+         (nexus.coord.x, nexus.coord.y, nexus.coord.z, nexus.minfluence,
+          nexus.maxfluence, nexus.radius, nexus.diffuse)
+      )
+      self.conn.commit()
 
 class Nexus:
    
@@ -52,7 +96,9 @@ def main():
    radius = 20
    
    nexus1 = Nexus(minfluence, maxfluence, radius, 1)
+   nexus1.coord = Coord(1, 2, 3)
    nexus2 = Nexus(minfluence, maxfluence, radius, 0.1)
+   nexus2.coord = Coord(-3, 4, 7)
    nexus3 = Nexus(minfluence, maxfluence, radius, 5)
    
    for nexus in [nexus1, nexus2, nexus3]:
@@ -60,6 +106,14 @@ def main():
       assert nexus.influence(radius) == minfluence
       assert nexus.influence(radius + 1) == 0
    
+   db = Database()
+   db.clear()
+   db.put_nexus(nexus1)
+   db.put_nexus(nexus2)
+   db.put_nexus(nexus3)
+   print(db.get_nexus(Coord(0, 0, 0)).diffuse)
+   
+   '''
    x = np.arange(0, radius + 2, 0.1)
    y1 = np.array([nexus1.influence(dist=xi) for xi in x])
    y2 = np.array([nexus2.influence(dist=xi) for xi in x])
@@ -72,6 +126,7 @@ def main():
    plt.plot(x, y3)
    plt.legend(['Nexus1', 'Nexus2', 'Nexus3'])
    plt.show()
+   '''
 
 if __name__ == '__main__':
    main()
